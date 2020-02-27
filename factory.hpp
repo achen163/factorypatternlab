0ba58b77048bf93478c045f87f587a9d2c921673
Cyoo9 <tyo002@ucr.edu>
2020-02-27 01:33:34 -0800

    delete googletest

--- factory.hpp
+++ factory.hpp
@@ -1,16 +1,27 @@
 #ifndef __FACTORY_HPP__
 #define __FACTORY_HPP__
 #include "base.hpp"
+#include <boost/tokenizer.hpp>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+#include <stack>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std; 
+
 class Factory : public Base {
 	public:
-		Base* createAdd();
-		Base* createSub();
-		Base* createMult();
-		Base* createDiv();
-		Base* createPow();
+		Base* createAdd(Base*, Base*);
+		Base* createSub(Base*, Base*);
+		Base* createMult(Base*, Base*);
+		Base* createDiv(Base*, Base*);
 		
-		void parse();
+		Base* parse(string);
+		void execute(Base*);
 	private:
 		Base* root;
+		Base* left;
 };
 #endif
